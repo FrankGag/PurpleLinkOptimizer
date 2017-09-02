be15acd4b09f0375c30fceaaa6172c9ea74902a3
François Gagnon <franky771@gmail.com>
2017-09-01 23:41:53 -0400

    Show history related to current tab url

--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
-﻿chrome.history.search({
-        'text': ''
-    },
-    function (historyItems) {
+﻿function searchHistory(url) {
+    var queryInfo = {
+        'text': url
+    };
+    chrome.history.search(queryInfo, function (historyItems) {
         console.log(historyItems);
     });
+}
+
+function getCurrentTabUrl(callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        callback(url);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    getCurrentTabUrl(searchHistory);
+});
