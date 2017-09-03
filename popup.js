341cfac2aa8eff8fffd9132c7bcbde62e480c887
François Gagnon <franky771@gmail.com>
2017-09-02 20:25:10 -0400

    Fix bugs and styling

--- popup.js
+++ popup.js
@@ -1,13 +1,18 @@
 ﻿var visits = [];
+function url_domain(data) {
+    var a = document.createElement('a');
+    a.href = data;
+    return a.hostname;
+}
+
 function getMoreVisitInfo(event) {
-    console.log(event);
-    var card = event.target;
+    var cardHeader = event.target;
     //Get the parent card
-    while (card.className !== 'card-header') {
-        card = card.parentElement;
+    while (cardHeader.className !== 'card-header') {
+        cardHeader = cardHeader.parentElement;
     }
     //Get the index of the card
-    var elemIndex = parseInt(card.id.replace('heading', ''));
+    var elemIndex = parseInt(cardHeader.id.replace('heading', ''));
     var currentVisit = visits[elemIndex];
     var millisecondsPerWeek = 1000 * 60 * 20;
     var endTime = currentVisit.visitTime + millisecondsPerWeek;
@@ -16,18 +21,25 @@ function getMoreVisitInfo(event) {
         'startTime': currentVisit.visitTime,
         'endTime': endTime
     };
-    //Get all history items in the 20 mins following the visit of the page
-    chrome.history.search(queryInfo, function (historyItems) {
-        //Get the card's table
-        var table = card.nextElementSibling.firstElementChild.firstElementChild;
-        for (var i = 0; i < historyItems.length; i++) {
-            var historyItem = historyItems[i];
-            var row = table.insertRow(1);
-            var cell1 = row.insertCell(0);
-            if (historyItem.title.length === 0) historyItem.title = historyItem.url
-            cell1.innerHTML = '<a  title="' + historyItem.url + '" href="' + historyItem.url + '">' + historyItem.title + '</a>';
-        }
-    });
+    var table = cardHeader.nextElementSibling.getElementsByTagName('tbody')[0];
+    if (table.rows.length == 0) {
+        //Get all history items in the 20 mins following the visit of the page
+        chrome.history.search(queryInfo, function (historyItems) {
+            //Get the card's table
+            for (var i = 0; i < historyItems.length; i++) {
+                var historyItem = historyItems[i];
+                if (historyItem.title.length === 0)
+                    historyItem.title = historyItem.url;
+
+                var row = table.insertRow(i);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+
+                cell1.innerHTML = url_domain(historyItem.url);
+                cell2.innerHTML = '<a target="_blank" title="' + historyItem.url + '" href="' + historyItem.url + '">' + historyItem.title + '</a>';
+            }
+        });
+    }
 }
 
 function searchHistory(url) {
@@ -51,15 +63,15 @@ function searchHistory(url) {
             html += '</div>';
             html += '<div id="collapse' + index + '" class="collapse" role="tabpanel" aria-labelledby="heading' + index + '" data-parent="#accordion">';
             html += '<div class="card-body">';
-            html += '<table class="table table-striped table-hover table-sm">';
+            html += '<table class="table table-hover table-bordered">';
             html += '<thead>';
             html += '<tr>';
-            html += '<th>';
-            html += '</th>';
-            html += '<th>';
-            html += '</th>';
+            html += '<th style="width:30%">Site</th>';
+            html += '<th style="width:70%">Page</th>';
             html += '</tr>';
             html += '</thead>';
+            html += '<tbody>';
+            html += '</tbody>';
             html += '</table>';
             html += '</div>';
             html += '</div>';
